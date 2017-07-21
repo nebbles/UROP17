1a548ae226343ebc2836dfa361a02c45ff050d5b
smb00123 <shreyuss@hotmail.com>
2017-07-21 16:23:00 +0100

    Added Threshold and Detect for maze

---
+++ Exercises/epxthreshdetect/build/CMakeFiles/epxthreshdetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epxthreshdetect.dir/epxthreshdetect.cpp.o"
+  "epxthreshdetect.pdb"
+  "epxthreshdetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/epxthreshdetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
